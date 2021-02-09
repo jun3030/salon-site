72ff023267f571a86dcf9cdfcde538cf6cbdabcc
Your Name <you@example.com>
2021-02-09 14:33:39 +0000

    save

---
+++ .history/app/javascript/app_20210209233218.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <p>スタッフはこちらから</p>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue"; //追記
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!!!!!"
+    }
+  },
+  components: {
+    Header //追記
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
