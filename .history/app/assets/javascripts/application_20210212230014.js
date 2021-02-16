279c32ec887bb1585c96b0456a4de15ef43f56af
Your Name <you@example.com>
2021-02-16 11:00:07 +0000

    calendar ututta

---
+++ .history/app/assets/javascripts/application_20210212230014.js
@@ -0,0 +1,14 @@
+//= require jquery # 追加
+//= require rails-ujs
+//= require moment # 追加
+//= require fullcalendar # 追加
+//= require activestorage
+//= require_tree .
+
+$(function () {
+
+  $('#calendar').fullCalendar({
+
+  });
+
+});
--- .history/app/javascript/packs/application_20210212235923.js
+++ .history/app/javascript/packs/application_20210212235923.js
@@ -4,16 +4,27 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("moment")
-require("fullcalendar")
-require("fullcalendar/locale-all")
-// 追加
-require ('calendar/main')
 
-$('#calendar').fullCalendar({});
+//ここまでは自動で生成された記述。
+//以下を追加
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+document.addEventListener('turbolinks:load', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, interactionPlugin ]
+  });
+
+  calendar.render();
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
