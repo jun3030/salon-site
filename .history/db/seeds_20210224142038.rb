f8183f3dba88e140fda14de0d5171138ca77c124
Your Name <you@example.com>
2021-02-24 13:35:25 +0000

    save

--- .history/db/seeds_20210224142038.rb
+++ .history/db/seeds_20210224142038.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(user_id:"matumoto3030", password:"password", password_confirmation:"password")
 Calendar.create!(calendar_name: "Rose", address:"東京都渋谷区", phone:"09057975695")
 Staff.create!(calendar_id: 1, name:"スタッフA", email:"staff@email.com", description: "元気いっぱいです！", staff_part: "店長")
+TaskCourse.create!(calendar_id: 1, title:"カット", description:"髪の毛を切る", course_time: 60, charge: "5000")
