3d6cbb2f65321157bf4e248312f9e42c85eb6885
Your Name <you@example.com>
2021-02-28 22:45:09 +0900

    before add calendar config

--- .history/app/models/calendar_20210122155011.rb
+++
@@ -1,9 +0,0 @@
-class Calendar < ApplicationRecord
-  has_one :calendar_config
-
-  validates :display_week_term, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
-  validates :end_date, presence: true, numericality: { only_integer: true }
-  validates :start_date, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-  validates :end_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
-  validates :start_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
-end
